a83483597a8d0c101fe340841c00739df0c8a1b7
Manoj Pathak <manoj.pathak1215@gmail.com>
2018-10-09 21:17:35 +0530

    Adding font

--- lib/pages/Author.dart
+++ lib/pages/Author.dart
@@ -7,10 +7,14 @@ class Author extends StatelessWidget {
         body: new GridView.count(crossAxisCount: 2, children: <Widget>[
           new Card(
               margin: new EdgeInsets.all(10.0),
-              child: new Center(child: new Text('Poet 1'))),
+              child: new Center(
+                  child: new Text(
+                'Poet 1',
+                style: new TextStyle(fontFamily: 'Muli', fontSize: 30.0),
+              ))),
           new Card(
               margin: new EdgeInsets.all(10.0),
-              child: new Center(child: new Text('Poet 1'))),
+              child: new Center(child: new Text('Poet 1', style: new TextStyle(fontSize: 30.0),))),
           new Card(
               margin: new EdgeInsets.all(10.0),
               child: new Center(child: new Text('Poet 1'))),
